b3d5f6e3254196afaad8887c1eb9604b0c4e62d0
Fooklnwza007-agent <Jeerapat.phum@ku.th>
2025-10-15 16:27:51 +0700

    Add files via upload

---
+++ DormNew/src/main/java/com/mycompany/dormnew/DormNew.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dormnew;
+
+import com.mycompany.dormnew.GUI.*;
+
+
+/**
+ *
+ * @author AVS_KTB
+ */
+public class DormNew {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
